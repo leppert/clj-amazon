473b29fa3ec464148eed3867cb30eeb2c0bae13d
Rory Gibson <rory.gibson@brightnorth.co.uk>
2016-01-28 16:30:55 +0000

    Making suitable for an initial clojars push

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jakemcc/clj-amazon "0.3.0-SNAPSHOT"
+(defproject org.clojars.rorygibson/clj-amazon "0.3.0-SNAPSHOT"
   :description "Clojure bindings for the Amazon Product Advertising API."
   :url "http://github.com/rorygibson/clj-amazon/"
   :license {:name "Eclipse Public License - v 1.0"
@@ -6,9 +6,11 @@
             :distribution :repo
             :comments "same as Clojure"}
   :plugins [[lein-autodoc "0.9.0"]]
+  :exclusions [org.clojure/clojure]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "2.0.1"]]
-  :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
-                                      :username :gpg :password :gpg}]
-                        ["releases" {:url "https://clojars.org/repo"
-                                     :username :gpg :password :gpg}]])
+  :profiles {
+             :dev {
+                   :dependencies [[org.clojure/clojure "1.7.0"]]
+                   }
+             })
